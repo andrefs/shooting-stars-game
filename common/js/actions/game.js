638160ef70ed304d4e0d9b57f4301c9cdc52cf80
André Santos <andrefs@andrefs.com>
2017-10-25 00:20:57 +0100

    Wrap around when finishing a game

--- common/js/actions/game.js
+++ common/js/actions/game.js
@@ -1,9 +1,14 @@
 import {
   FETCH_GAME_REQUEST, FETCH_GAME_SUCCESS, FETCH_GAME_FAILURE,
-  POST_PICK_REQUEST, POST_PICK_SUCCESS, POST_PICK_FAILURE
+  POST_PICK_REQUEST, POST_PICK_SUCCESS, POST_PICK_FAILURE,
+  CREATE_GAME_REQUEST, CREATE_GAME_SUCCESS, CREATE_GAME_FAILURE,
 } from '../constants';
-import {getGameInstance, postPlayerPick} from 'lib/api';
+import {getGameInstance, postPlayerPick, createNewGame} from 'lib/api';
 import generateActionCreator from 'lib/generateActionCreator';
+
+export const createGameRequest = generateActionCreator(CREATE_GAME_REQUEST);
+export const createGameSuccess = generateActionCreator(CREATE_GAME_SUCCESS, 'game');
+export const createGameFailure = generateActionCreator(CREATE_GAME_FAILURE, 'error');
 
 export const fetchGameRequest = generateActionCreator(FETCH_GAME_REQUEST);
 export const fetchGameSuccess = generateActionCreator(FETCH_GAME_SUCCESS, 'game');
@@ -13,6 +18,20 @@ export const postPickRequest = generateActionCreator(POST_PICK_REQUEST);
 export const postPickSuccess = generateActionCreator(POST_PICK_SUCCESS, 'game');
 export const postPickFailure = generateActionCreator(POST_PICK_FAILURE, 'error');
 
+export const createGame = () => {
+  return async (dispatch) => {
+    dispatch(createGameRequest());
+    try {
+      const response = await createNewGame();
+      const game = await response.json();
+
+      dispatch(createGameSuccess(game));
+    } catch (e) {
+      dispatch(createGameFailure(e.message));
+    }
+  };
+};
+
 export const fetchGame = () => {
   return async (dispatch) => {
     dispatch(fetchGameRequest());
@@ -20,7 +39,11 @@ export const fetchGame = () => {
       const response = await getGameInstance();
       const game = await response.json();
 
-      dispatch(fetchGameSuccess(game));
+      if(!game || game.isFinished){
+        dispatch(createGame());
+      } else {
+        dispatch(fetchGameSuccess(game));
+      }
     } catch (e) {
       dispatch(fetchGameFailure(e.message));
     }
@@ -35,7 +58,11 @@ export const postPick = pick => {
       const response = await postPlayerPick(pick);
       const game = await response.json();
 
-      dispatch(postPickSuccess(game));
+      if(!game || game.isFinished){
+        dispatch(createGame());
+      } else {
+        dispatch(postPickSuccess(game));
+      }
     } catch (e) {
       dispatch(postPickFailure(e.message));
     }
--- common/js/lib/api.js
+++ common/js/lib/api.js
@@ -4,7 +4,6 @@ const apiUrl = 'http://localhost:15111';
 
 // Overrides the fetch() method to add the base API url to the front.
 export const fetch = (url, params, ...rest) => {
-  console.log('XXXXXXXXXXXXXXXx 88', apiUrl + url, {mode: 'cors', ...params}, ...rest);
   return isomorphicFetch( apiUrl + url, {mode: 'cors', ...params}, ...rest);
 };
 
@@ -15,5 +14,10 @@ export const getGameInstance = (...params) => {
 
 export const postPlayerPick = (pick) => {
   const url = `/gameInstances/currentTurn/${pick}?userId=59ecfd6c28318d1a0d04a189`;
+  return fetch(url, {method: 'POST'});
+};
+
+export const createNewGame = () => {
+  const url = '/gameInstances/current/?userId=59ecfd6c28318d1a0d04a189';
   return fetch(url, {method: 'POST'});
 };
