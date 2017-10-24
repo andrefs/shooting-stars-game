0a4acd2f5c0221fd20c449498b8a32938abb2d0f
André Santos <andrefs@andrefs.com>
2017-10-24 20:18:23 +0100

    Minor stuff

--- common/js/actions/game.js
+++ common/js/actions/game.js
@@ -1,26 +1,46 @@
 import {
-  FETCH_GAME_REQUEST, FETCH_GAME_SUCCESS, FETCH_GAME_FAILURE
+  FETCH_GAME_REQUEST, FETCH_GAME_SUCCESS, FETCH_GAME_FAILURE,
+  POST_PICK_REQUEST, POST_PICK_SUCCESS, POST_PICK_FAILURE
 } from '../constants';
-import {fetch} from 'lib/api';
+// import {fetch} from 'lib/api';
 import generateActionCreator from 'lib/generateActionCreator';
-
 
 export const fetchGameRequest = generateActionCreator(FETCH_GAME_REQUEST);
 export const fetchGameSuccess = generateActionCreator(FETCH_GAME_SUCCESS, 'game');
 export const fetchGameFailure = generateActionCreator(FETCH_GAME_FAILURE, 'error');
 
+export const postPickRequest = generateActionCreator(POST_PICK_REQUEST);
+export const postPickSuccess = generateActionCreator(POST_PICK_SUCCESS, 'game');
+export const postPickFailure = generateActionCreator(POST_PICK_FAILURE, 'error');
+
 export const fetchGame = () => {
-  console.log('XXXXXXXXXXXXXXXXx 3');
   return async (dispatch) => {
     dispatch(fetchGameRequest());
     try {
       // const response = await fetch('/api/todos', { method: 'GET' });
       // const game = await response.json();
 
-      const game = {'_id':'59ed078e2736f754f8aed035','modified':'2017-10-22T21:11:49.520Z','created':'2017-10-22T21:03:10.922Z','isFinished':false,'currentScore':{'sync':350,'async':350,'bot':50},'turns':{'previous':[{'_id':'59ed078e2736f754f8aed038','triple':{'_id':'59ecfd5d28318d1a0d04a0a2','randomSeq':'0108e468f78d2d0e022f5deb9abf75bc','highestItemId':8,'itemA':'59ecfd5c28318d1a0d04a080','itemB':'59ecfd5c28318d1a0d04a081','itemC':{'_id':'59ecfd5c28318d1a0d04a084','id':8,'imageUrl':'photos/brunomars.jpg','name':'Bruno Mars','modified':'2017-10-22T20:19:40.927Z','created':'2017-10-22T20:19:40.927Z','__v':0},'modified':'2017-10-22T20:19:41.776Z','created':'2017-10-22T20:19:41.776Z','__v':0},'modified':'2017-10-22T21:08:20.805Z','created':'2017-10-22T21:03:10.996Z','score':{'modified':null,'created':null,'_id':'59ed078e2736f754f8aed037','bot':0,'async':50,'sync':50},'moves':{'modified':null,'created':null,'_id':'59ed078e2736f754f8aed036','sync':'AB','async':'AB','bot':'BC'},'__v':0},{'_id':'59ed08c4a7e867528b93f9da','triple':{'_id':'59ecfd5d28318d1a0d04a0a2','randomSeq':'0108e468f78d2d0e022f5deb9abf75bc','highestItemId':8,'itemA':'59ecfd5c28318d1a0d04a080','itemB':'59ecfd5c28318d1a0d04a081','itemC':{'_id':'59ecfd5c28318d1a0d04a084','id':8,'imageUrl':'photos/brunomars.jpg','name':'Bruno Mars','modified':'2017-10-22T20:19:40.927Z','created':'2017-10-22T20:19:40.927Z','__v':0},'modified':'2017-10-22T20:19:41.776Z','created':'2017-10-22T20:19:41.776Z','__v':0},'modified':'2017-10-22T21:08:21.753Z','created':'2017-10-22T21:08:20.832Z','score':{'modified':null,'created':null,'_id':'59ed08c4a7e867528b93f9d9','bot':0,'async':50,'sync':50},'moves':{'modified':null,'created':null,'_id':'59ed08c4a7e867528b93f9d8','sync':'AB','async':'AB','bot':'BC'},'__v':0}],'current':{'_id':'59ed0995a7e867528b93fa58','triple':{'_id':'59ecfd5d28318d1a0d04a100','itemA':{'_id':'59ecfd5c28318d1a0d04a080','id':3,'imageUrl':'https://www.famousbirthdays.com/headshots/justin-bieber-2.jpg','name':'Justin Bieber','modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z','__v':0},'itemB':{'_id':'59ecfd5c28318d1a0d04a081','id':5,'imageUrl':'http://www.iol.pt/multimedia/oratvi/multimedia/imagem/id/13940583/800','name':'Carlos Paião','modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z','__v':0},'itemC':{'_id':'59ecfd5c28318d1a0d04a083','id':9,'imageUrl':'https://images.vice.com/noisey/content-images/article/charles-bradley-interview-2014-strictly-reserved-for-you/Screen-Shot-2014-12-02-at-1-48-13-PM.jpg?crop=1xw:0.8987676056338029xh;center,center&resize=1050:*','name':'Charles Bradley','modified':'2017-10-22T20:19:40.927Z','created':'2017-10-22T20:19:40.927Z','__v':0}}}},'players':{'sync':{'_id':'59ecfd6d28318d1a0d04a18b','username':'user4@example.com'},'async':{'_id':'59ecfd6c28318d1a0d04a188','username':'user1@example.com'}},'__v':7};
+      const game = {'_id':'59ef89349227064ec5716ebb','__v':1,'modified':'2017-10-24T18:41:10.852Z','created':'2017-10-24T18:40:52.073Z','isFinished':false,'currentScore':{'sync':50,'async':50,'bot':0},'turns':{'current':{'_id':'59ef89469227064ec5716ec5','triple':{'_id':'59ecfd5d28318d1a0d04a0bd','randomSeq':'8c8e9ff51496d5fc7c2d7e07dbc8db53','highestItemId':10,'itemA':{'_id':'59ecfd5c28318d1a0d04a080','id':3,'imageUrl':'photos/prince.jpg','name':'Prince','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'itemB':{'_id':'59ecfd5c28318d1a0d04a084','id':8,'imageUrl':'photos/brunomars.jpg','name':'Bruno Mars','__v':0,'modified':'2017-10-22T20:19:40.927Z','created':'2017-10-22T20:19:40.927Z'},'itemC':{'_id':'59ecfd5c28318d1a0d04a07c','id':10,'imageUrl':'photos/gaga.jpg','name':'Lady Gaga','__v':0,'modified':'2017-10-22T20:19:40.925Z','created':'2017-10-22T20:19:40.925Z'},'__v':0,'modified':'2017-10-22T20:19:41.977Z','created':'2017-10-22T20:19:41.978Z'}},'previous':[{'_id':'59ef89349227064ec5716ebe','triple':{'_id':'59ecfd5d28318d1a0d04a165','randomSeq':'88b30f69aa41afa808becaa0614d86e8','highestItemId':10,'itemA':{'_id':'59ecfd5c28318d1a0d04a07b','id':1,'imageUrl':'photos/madonna.jpg','name':'Madonna','__v':0,'modified':'2017-10-22T20:19:40.921Z','created':'2017-10-22T20:19:40.921Z'},'itemB':{'_id':'59ecfd5c28318d1a0d04a082','id':7,'imageUrl':'photos/amy.jpeg','name':'Amy Whinehouse','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'itemC':{'_id':'59ecfd5c28318d1a0d04a07c','id':10,'imageUrl':'photos/gaga.jpg','name':'Lady Gaga','__v':0,'modified':'2017-10-22T20:19:40.925Z','created':'2017-10-22T20:19:40.925Z'},'__v':0,'modified':'2017-10-22T20:19:42.251Z','created':'2017-10-22T20:19:42.251Z'},'__v':0,'modified':'2017-10-24T18:41:10.812Z','created':'2017-10-24T18:40:52.152Z','score':{'modified':null,'created':null,'_id':'59ef89349227064ec5716ebd','bot':0,'async':50,'sync':50},'moves':{'sync':'AB','async':'AB','bot':'AC','modified':null,'created':null,'_id':'59ef89349227064ec5716ebc'}}]},'players':{'sync':{'_id':'59ecfd6c28318d1a0d04a18a','username':'user3@example.com'},'async':null}};
       dispatch(fetchGameSuccess(game));
     } catch (e) {
       dispatch(fetchGameFailure(e.message));
+    }
+  };
+};
+
+export const postPick = pick => {
+  return async (dispatch) => {
+
+    dispatch(postPickRequest(pick));
+    try {
+    //   const response = await fetch('/gameInstances/current', {method:'POST'});
+    //   const game = await response.json();
+
+      const game = {'_id':'59ef89349227064ec5716ebb','__v':2,'modified':'2017-10-24T18:43:42.580Z','created':'2017-10-24T18:40:52.073Z','isFinished':false,'currentScore':{'bot':0,'async':100,'sync':100},'turns':{'current':{'_id':'59ef89de9227064ec5716ed4','triple':{'_id':'59ecfd5d28318d1a0d04a112','randomSeq':'8d1abf4f826cf0858b70cfe805676118','highestItemId':7,'itemA':{'_id':'59ecfd5c28318d1a0d04a07d','id':2,'imageUrl':'photos/queen.jpg','name':'Freddie Mercury (Queen)','__v':0,'modified':'2017-10-22T20:19:40.925Z','created':'2017-10-22T20:19:40.925Z'},'itemB':{'_id':'59ecfd5c28318d1a0d04a07e','id':4,'imageUrl':'photos/bono.png','name':'Bono (U2)','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'itemC':{'_id':'59ecfd5c28318d1a0d04a082','id':7,'imageUrl':'photos/amy.jpeg','name':'Amy Whinehouse','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'__v':0,'modified':'2017-10-22T20:19:42.330Z','created':'2017-10-22T20:19:42.330Z'}},'previous':[{'_id':'59ef89349227064ec5716ebe','triple':{'_id':'59ecfd5d28318d1a0d04a165','randomSeq':'88b30f69aa41afa808becaa0614d86e8','highestItemId':10,'itemA':{'_id':'59ecfd5c28318d1a0d04a07b','id':1,'imageUrl':'photos/madonna.jpg','name':'Madonna','__v':0,'modified':'2017-10-22T20:19:40.921Z','created':'2017-10-22T20:19:40.921Z'},'itemB':{'_id':'59ecfd5c28318d1a0d04a082','id':7,'imageUrl':'photos/amy.jpeg','name':'Amy Whinehouse','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'itemC':{'_id':'59ecfd5c28318d1a0d04a07c','id':10,'imageUrl':'photos/gaga.jpg','name':'Lady Gaga','__v':0,'modified':'2017-10-22T20:19:40.925Z','created':'2017-10-22T20:19:40.925Z'},'__v':0,'modified':'2017-10-22T20:19:42.251Z','created':'2017-10-22T20:19:42.251Z'},'__v':0,'modified':'2017-10-24T18:41:10.812Z','created':'2017-10-24T18:40:52.152Z','score':{'modified':null,'created':null,'_id':'59ef89349227064ec5716ebd','bot':0,'async':50,'sync':50},'moves':{'sync':'AB','async':'AB','bot':'AC','modified':null,'created':null,'_id':'59ef89349227064ec5716ebc'}},{'_id':'59ef89469227064ec5716ec5','triple':{'_id':'59ecfd5d28318d1a0d04a0bd','randomSeq':'8c8e9ff51496d5fc7c2d7e07dbc8db53','highestItemId':10,'itemA':{'_id':'59ecfd5c28318d1a0d04a080','id':3,'imageUrl':'photos/prince.jpg','name':'Prince','__v':0,'modified':'2017-10-22T20:19:40.926Z','created':'2017-10-22T20:19:40.926Z'},'itemB':{'_id':'59ecfd5c28318d1a0d04a084','id':8,'imageUrl':'photos/brunomars.jpg','name':'Bruno Mars','__v':0,'modified':'2017-10-22T20:19:40.927Z','created':'2017-10-22T20:19:40.927Z'},'itemC':{'_id':'59ecfd5c28318d1a0d04a07c','id':10,'imageUrl':'photos/gaga.jpg','name':'Lady Gaga','__v':0,'modified':'2017-10-22T20:19:40.925Z','created':'2017-10-22T20:19:40.925Z'},'__v':0,'modified':'2017-10-22T20:19:41.977Z','created':'2017-10-22T20:19:41.978Z'},'__v':0,'modified':'2017-10-24T18:43:42.540Z','created':'2017-10-24T18:41:10.835Z','score':{'modified':null,'created':null,'_id':'59ef89469227064ec5716ec4','bot':0,'async':50,'sync':50},'moves':{'sync':'AB','async':'AB','bot':'AC','modified':null,'created':null,'_id':'59ef89469227064ec5716ec3'}}]},'players':{'sync':{'_id':'59ecfd6c28318d1a0d04a18a','username':'user3@example.com'},'async':null}};
+
+      dispatch(postPickSuccess(game));
+    } catch (e) {
+      dispatch(postPickFailure(e.message));
     }
   };
 };
