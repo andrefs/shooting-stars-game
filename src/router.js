69c0094c220091ef219ce34f2fece3eb53ae660e
André Santos <andrefs@andrefs.com>
2018-08-22 16:17:51 +0100

    Add views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+// import Home from './views/Home.vue';
+import Game from './views/Game.vue';
+import Login from './views/Login.vue';
+import Register from './views/Register.vue';
 
 Vue.use(Router);
 
@@ -10,16 +13,37 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'welcome',
+      component: () => import(/* webpackChunkName: "about" */ './views/Welcome.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/game',
+      name: 'game',
+      component: Game,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+
+    // {
+    //   path: '/home',
+    //   name: 'home',
+    //   component: Home,
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
   ],
 });
