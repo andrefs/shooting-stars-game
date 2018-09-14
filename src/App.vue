06e682cc4fa1948637de624dafe6b51ada806bd3
André Santos <andrefs@andrefs.com>
2018-09-14 11:14:26 +0100

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
     <v-content>
       <router-view/>
     </v-content>
-    <v-footer :fixed="fixed" app>
+    <!--v-footer :fixed="fixed" app>
       <span>&copy; 2018</span>
-    </v-footer>
+    </v-footer -->
   </v-app>
 </template>
 
